b19c95dea918c6fad9bbceb04da841101261073c
Turquoise Hexagon Sun <shanekiantiu@gmail.com>
2022-10-15 09:27:11 +0800

    Initial initial commit

---
+++ src/main/java/com/example/hr_request_tracker/hr_request_tracker/HrRequestTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hr_request_tracker.hr_request_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrRequestTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrRequestTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hr_request_tracker/hr_request_tracker/HrRequestTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hr_request_tracker.hr_request_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrRequestTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
