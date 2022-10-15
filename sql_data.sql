a358f314153bca57fae692b2b34903f8e0cd2a85
Gilben Ferolino <gilbenferolinoz@gmail.com>
2022-10-15 11:04:18 +0800

    sql_data script

---
+++ sql_data.sql
@@ -0,0 +1,6 @@
+INSERT into ticket_module(ticketid, asignee,status,subject,description,tracker) values (001, "John Doe","Active", "Request for salary increase", "","IT Dept");
+INSERT into ticket_module(ticketid, asignee,status,subject,description,tracker) values (002, "Thor Java","Cancelled", "Request for leave", "Would like to take a vacation" ,"IT Dept");
+INSERT into ticket_module(ticketid, asignee,status,subject,description,tracker) values (003, "Gad Gadiane","Inactive", "Conflict with colleague", "Keeps stealing my lunch", "Finance Dept");
+INSERT into ticket_module(ticketid, asignee,status,subject,description,tracker) values (004, "Maria Gad","Declined", "Request for transfer of management", "Would like to manage different area", "Management");
+INSERT into ticket_module(ticketid, asignee,status,subject,description,tracker) values (005, "John Doe","Approved", "Request for team building","New training and seminars", "IT Dept");
+
