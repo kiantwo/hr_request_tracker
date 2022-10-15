a444ab7621b92b415f1ffd20930cac8e8415a427
Turquoise Hexagon Sun <shanekiantiu@gmail.com>
2022-10-15 11:48:37 +0800

    Update TicketRepository argument names

--- src/main/java/com/example/hr_request_tracker/hr_request_tracker/repository/ITicketRepository.java
+++ src/main/java/com/example/hr_request_tracker/hr_request_tracker/repository/ITicketRepository.java
@@ -8,8 +8,8 @@ import com.example.hr_request_tracker.hr_request_tracker.model.Ticket;
 public interface ITicketRepository {
 	public Ticket findById(final int id);
 	public List<Ticket> findAll();
-	public int save(final Ticket user);
-	public int updateByID(final Ticket user);
+	public int save(final Ticket ticket);
+	public int updateByID(final Ticket ticket);
 	public int updateAssignee(final int id, final String assignee);
 	public int updateStatus(final int id, final String status);
 	public int deleteByID(final int id);
