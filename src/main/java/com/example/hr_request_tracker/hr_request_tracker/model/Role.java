d70469ec4b9c8cab5d0c5bd96f997a16d404031a
Turquoise Hexagon Sun <shanekiantiu@gmail.com>
2022-10-22 10:27:22 +0800

    Add partial role controller, service, repository files

---
+++ src/main/java/com/example/hr_request_tracker/hr_request_tracker/model/Role.java
@@ -0,0 +1,5 @@
+package com.example.hr_request_tracker.hr_request_tracker.model;
+
+public class Role {
+	public Role() {}
+}
--- src/main/java/com/example/hr_request_tracker/hr_request_tracker/repository/IUserRepository.java
+++ src/main/java/com/example/hr_request_tracker/hr_request_tracker/repository/IUserRepository.java
@@ -1,13 +1,13 @@
 package com.example.hr_request_tracker.hr_request_tracker.repository;
 
-import java.io.BufferedReader;
+import java.util.List;
 
 import com.example.hr_request_tracker.hr_request_tracker.model.User;
 
 public interface IUserRepository {
-	public String findById(final int id);
-	public String findAll();
-	public int save(final BufferedReader body);
-	public int update(final User user);
-	public int deleteById(final int id);
+	public User findById(final int id);
+	public List<User> findAll();
+	public int save(final User user);
+	public int updateByID(final User user);
+	public int deleteByID(final int id);	
 }
