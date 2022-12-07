494c518de7cbb244a6d341aa00a9c44207043382
Kian <shanekiantiu@gmail.com>
2022-12-07 08:16:52 +0800

    Fix authenticate repository method

--- src/main/java/com/example/hr_request_tracker/hr_request_tracker/authentication/repository/ITokenRepository.java
+++ src/main/java/com/example/hr_request_tracker/hr_request_tracker/authentication/repository/ITokenRepository.java
@@ -3,16 +3,12 @@ package com.example.hr_request_tracker.hr_request_tracker.authentication.reposit
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.hr_request_tracker.hr_request_tracker.authentication.model.Token;
 
 @Repository
-public interface ITokenRepository extends JpaRepository<Token, Integer> {
-//	@Query("select t from Token t where t.user.userID = :userId order by tokenID desc")
-//	public Optional<Token> authenticate(@Param("userId") Integer userId);
-	
+public interface ITokenRepository extends JpaRepository<Token, Integer> {	
 	public Optional<Token> findTopByUserUserIDOrderByTokenIDDesc(@Param("userId") Integer userId);
 }
